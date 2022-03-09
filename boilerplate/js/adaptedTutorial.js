05f3332a350730a638e53d0826e53b56ecd80bfa
lhwcodes <98495495+lhwcodes@users.noreply.github.com>
2022-03-08 19:01:49 -0600

    Activity 6
    Finally

--- boilerplate/js/adaptedTutorial.js
+++ boilerplate/js/adaptedTutorial.js
@@ -1,12 +1,15 @@
-/* Map of GeoJSON data from MegaCities.geojson */
-//declare map var in global scope
+//declare map variable globally so all functions have access
 var map;
-//function to instantiate the Leaflet map
+var minValue;
+
+//step 1 create map
 function createMap(){
+
     //create the map
     map = L.map('map', {
-        center: [20, 0],
-        zoom: 2
+        center: [0, 0],
+        zoom: 2,
+        scrollWheelZoom: true
     });
 
     //add OSM base tilelayer
@@ -15,86 +18,200 @@ function createMap(){
     }).addTo(map);
 
     //call getData function
-    getData()
-}
-//added at Example 2.3 line 20...function to attach popups to each mapped feature
-function onEachFeature(feature, layer) {
-    //no property named popupContent; instead, create html string with all properties
-    var popupContent = "";
-    if (feature.properties) {
-        //loop to add feature property names and values to html string
-        for (var property in feature.properties){
-            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
-        }
-        layer.bindPopup(popupContent);
-    };
-
+    getData(map);
 };
 
-//function to retrieve the data and place it on the map
+function calculateMinValue(data){
+    //create empty array to store all data values
+    var allValues = [];
+    //loop through each city
+    for(var city of data.features){
+        //loop through each year
+        for(var year = 1985; year <= 2015; year+=5){
+              //get population for current year
+              var value = city.properties["Pop_"+ String(year)];
+              
+              //add value to array
+              allValues.push(value);
+        }
+    }
+    //get minimum value of our array
+    var minValue = Math.min(...allValues)
+
+    return minValue;
+}
+
+//calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+    //constant factor adjusts symbol sizes evenly
+    var minRadius = 10;
+    //Flannery Apperance Compensation formula
+    var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
+
+    return radius;
+};
+
+//Step 3: Add circle markers for point features to the map
+//function to convert markers to circle markers
+function pointToLayer(feature, latlng, attributes){
+    //Determine which attribute to visualize with proportional symbols
+    var attribute = attributes[0];
+
+    //create marker options
+    var options = {
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    //For each feature, determine its value for the selected attribute
+    var attValue = Number(feature.properties[attribute]);
+
+    //Give each feature's circle marker a radius based on its attribute value
+    options.radius = calcPropRadius(attValue);
+
+    //create circle marker layer
+    var layer = L.circleMarker(latlng, options);
+
+    //build popup content string starting with city...Example 2.1 line 24
+    var popupContent = "<p><b>City:</b> " + feature.properties.City + "</p>";
+
+    //add formatted attribute to popup content string
+    var year = attribute.split("_")[1];
+    
+    popupContent += "<p><b>Population in " + year + ":</b> " + feature.properties[attribute] + " million</p>";
+
+    //bind the popup to the circle marker
+    layer.bindPopup(popupContent);
+    
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+    
+};
+
+//Add circle markers for point features to the map
+function createPropSymbols(data, attributes){
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: function(feature, latlng){ 
+            return pointToLayer(feature, latlng, attributes);
+        }
+    }).addTo(map);
+};
+
+//Step 3: build an attributes array from the data
+function processData(data){
+    //empty array to hold attributes
+    var attributes = [];
+
+    //properties of the first feature in the dataset
+    var properties = data.features[0].properties;
+
+    //push each attribute name into attributes array
+    for (var attribute in properties){
+        //only take attributes with population values
+        if (attribute.indexOf("Pop") > -1){
+            attributes.push(attribute);
+        };
+    };
+
+    //check result
+    
+
+    return attributes;
+};
+
+//Step 1: Create new sequence controls
+function createSequenceControls(attributes){
+    //create range input element (slider)
+    var slider = "<input class='range-slider' type='range'></input>";
+    document.querySelector("#panel").insertAdjacentHTML('beforeend',slider);
+
+    document.querySelector('.range-slider').max = 6;
+    document.querySelector('.range-slider').min = 0;
+    document.querySelector('.range-slider').value = 0;
+    document.querySelector('.range-slider').step = 1;
+    //below Example 3.6...add step buttons
+    document.querySelector('#panel').insertAdjacentHTML('afterbegin','<button class="step" id="reverse"></button>');
+    document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="forward"></button>');
+
+    document.querySelector('#reverse').insertAdjacentHTML('beforeend','<img src="img/reverse.png">');
+    document.querySelector('#forward').insertAdjacentHTML('beforeend','<img src="img/forward.png">');
+
+     //Step 5: click listener for buttons
+     document.querySelectorAll('.step').forEach(function(step){
+        step.addEventListener("click", function(){
+            var index = document.querySelector('.range-slider').value;
+            
+            //Step 6: increment or decrement depending on button clicked
+            if (step.id == 'forward'){
+                index++;
+                //Step 7: if past the last attribute, wrap around to first attribute
+                index = index > 6 ? 0 : index;
+            } else if (step.id == 'reverse'){
+                index--;
+                //Step 7: if past the first attribute, wrap around to last attribute
+                index = index < 0 ? 6 : index;
+            };
+
+            //Step 8: update slider
+            document.querySelector('.range-slider').value = index;
+
+            updatePropSymbols(attributes[index]);
+            
+        })
+    })
+    //Step 5: input listener for slider
+    document.querySelector('.range-slider').addEventListener('input', function(){            
+        var index = this.value;
+        updatePropSymbols(attributes[index]);
+        
+    });
+};
+
+//Step 10: Resize proportional symbols according to new attribute values
+function updatePropSymbols(attribute){
+    map.eachLayer(function(layer){
+        if (layer.feature && layer.feature.properties[attribute]){
+            //update the layer style and popup
+            //access feature properties
+            var props = layer.feature.properties;
+
+            //update each feature's radius based on new attribute values
+            var radius = calcPropRadius(props[attribute]);
+            layer.setRadius(radius);
+
+            //add city to popup content string
+            var popupContent = "<p><b>City:</b> " + props.City + "</p>";
+
+            //add formatted attribute to panel content string
+            var year = attribute.split("_")[1];
+            popupContent += "<p><b>Population in " + year + ":</b> " + props[attribute] + " million</p>";
+
+            //update popup content            
+            popup = layer.getPopup();            
+            popup.setContent(popupContent).update();
+        };
+    });
+};
+
+//Step 2: Import GeoJSON data
 function getData(){
     //load the data
     fetch("data/MegaCities.geojson")
         .then(function(response){
             return response.json();
         })
-        // .then(function(json){
-        //     //create a Leaflet GeoJSON layer and add it to the map
-        //     L.geoJson(json, {
-        //         onEachFeature: onEachFeature
-        //     }).addTo(map);
-        // }) 
-        .then(function(json) {
-                //create marker options
-                var geojsonMarkerOptions = {
-                    radius: 8,
-                    fillColor: "#ff7800",
-                    color: "#000",
-                    weight: 1,
-                    opacity: 1,
-                    fillOpacity: 0.8
-                };
-                //using the pointToLayer method to turn any group of point features into
-                //their own layer onn the map, using parameters from the markeroptions var
-                L.geoJSON(json, {
-                    pointToLayer: function (feature, latlng) {
-                        return L.circleMarker(latlng, geojsonMarkerOptions);
-                    },
-                    onEachFeature: onEachFeature
-                }).addTo(map);
-             })
-       
+        .then(function(json){
+            var attributes = processData(json);
+            //calculate minimum data value
+            minValue = calculateMinValue(json);
+            //call function to create proportional symbols
+            createPropSymbols(json, attributes);
+            createSequenceControls(attributes);
+        })
 };
 
 document.addEventListener('DOMContentLoaded',createMap)
-
-
-//creating a variable to store style for the lines
-    // var myStyle = {
-    //     "color": "#ff7800",
-    //     "weight": 5,
-    //     "opacity": 0.65
-    // };
-    //adding the style parameters to the lines before adding them to the map
-    // L.geoJSON(, {
-    //     style: myStyle
-    // }).addTo(map);
- 
-//creating a variable to store marker parameters 
-  
-//using onEachFeature method to check if each feature has properties, and 
-//specifically, popupContent. If it does, it binds the popup to the 
-//feature when it is clicked 
-
-    
-   
-    //the filter method is used here to cycle through the features of a specified list
-    //and checks the show_on_map property if the feature should be represented
-    // L.geoJSON(someFeatures, {
-    //     filter: function(feature, layer) {
-    //         return feature.properties.show_on_map;
-    //     }
-    // }).addTo(map);
-
-  
-  
--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,9 +1,10 @@
 var map;
+var minValue;
 //function to instantiate the Leaflet map
 function createMap(){
     //create the map
     map = L.map('map', {
-        center: [20, 0],
+        center: [0, 0],
         zoom: 2
     });
 
@@ -15,54 +16,260 @@ function createMap(){
     //call getData function
     getData()
 }
-//added at Example 2.3 line 20...function to attach popups to each mapped feature
-function onEachFeature(feature, layer) {
-    //no property named popupContent; instead, create html string with all properties
-    var popupContent = "";
-    if (feature.properties) {
-        //loop to add feature property names and values to html string
-        for (var property in feature.properties){
-            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+function calculateMinValue(data){
+    //create empty array to store all data values
+    var allValues = [];
+    //loop through each city
+    for(var state of data.features){
+        //loop through each year
+        for(var year = 2009; year <= 2021; year+=2){
+              //get population for current year
+              var value = Number(state.properties[String(year)]);
+              //add value to array
+              allValues.push(value);
         }
-        layer.bindPopup(popupContent);
+    }
+    //get minimum value of our array
+    var minValue = Math.min(...allValues)
+    
+
+    return minValue;
+}
+//calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+    //constant factor adjusts symbol sizes evenly
+    var minRadius = 5;
+    //Flannery Apperance Compensation formula
+    var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
+
+    return radius;
+};
+//Step 3: Add circle markers for point features to the map
+//function to convert markers to circle markers
+function pointToLayer(feature, latlng, attributes){
+    //Determine which attribute to visualize with proportional symbols
+    var attribute = attributes[0];
+
+    //create marker options
+    var options = {
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
     };
 
+    //For each feature, determine its value for the selected attribute
+    var attValue = Number(feature.properties[attribute]);
+
+    //Give each feature's circle marker a radius based on its attribute value
+    options.radius = calcPropRadius(attValue);
+
+    //create circle marker layer
+    var layer = L.circleMarker(latlng, options);
+
+    //build popup content string starting with city...Example 2.1 line 24
+    var popupContent = "<p><b>State:</b> " + feature.properties.State + "</p>";
+
+    //add formatted attribute to popup content string
+    var year = attribute;
+    
+    popupContent += "<p><b>Gross Sales of Hunting and Fishing Lisences in " + year + ":</b> " + feature.properties[attribute] + " million</p>";
+
+    //bind the popup to the circle marker
+    layer.bindPopup(popupContent);
+    
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+    
 };
 
-//function to retrieve the data and place it on the map
+//Add circle markers for point features to the map
+function createPropSymbols(data, attributes){
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: function(feature, latlng){
+            return pointToLayer(feature, latlng, attributes);
+        }
+
+    }).addTo(map);
+};
+
+//Step 3: build an attributes array from the data
+function processData(data){
+    //empty array to hold attributes
+    var attributes = [];
+
+    //properties of the first feature in the dataset
+    var properties = data.features[0].properties;
+
+    //push each attribute name into attributes array
+    for (var attribute in properties){
+        //only take attributes with population values
+        if (attribute.indexOf("20") > -1){
+            attributes.push(attribute);
+         
+        };
+    };
+
+    //check result
+    
+
+    return attributes;
+};
+
+//Step 1: Create new sequence controls
+function createSequenceControls(attributes){
+    //create range input element (slider)
+    var slider = "<input class='range-slider' type='range'></input>";
+    document.querySelector("#panel").insertAdjacentHTML('beforeend',slider);
+
+    document.querySelector('.range-slider').max = 6;
+    document.querySelector('.range-slider').min = 0;
+    document.querySelector('.range-slider').value = 0;
+    document.querySelector('.range-slider').step = 1;
+    //below Example 3.6...add step buttons
+    document.querySelector('#panel').insertAdjacentHTML('afterbegin','<button class="step" id="reverse"></button>');
+    document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="forward"></button>');
+
+    document.querySelector('#reverse').insertAdjacentHTML('beforeend','<img src="img/reverse.png">');
+    document.querySelector('#forward').insertAdjacentHTML('beforeend','<img src="img/forward.png">');
+
+     //Step 5: click listener for buttons
+     document.querySelectorAll('.step').forEach(function(step){
+        step.addEventListener("click", function(){
+            var index = document.querySelector('.range-slider').value;
+            
+            //Step 6: increment or decrement depending on button clicked
+            if (step.id == 'forward'){
+                index++;
+                //Step 7: if past the last attribute, wrap around to first attribute
+                index = index > 6 ? 0 : index;
+            } else if (step.id == 'reverse'){
+                index--;
+                //Step 7: if past the first attribute, wrap around to last attribute
+                index = index < 0 ? 6 : index;
+            };
+
+            //Step 8: update slider
+            document.querySelector('.range-slider').value = index;
+
+            updatePropSymbols(attributes[index]);
+            
+        })
+    })
+    //Step 5: input listener for slider
+    document.querySelector('.range-slider').addEventListener('input', function(){            
+        var index = this.value;
+        updatePropSymbols(attributes[index]);
+        
+    });
+};
+//Step 10: Resize proportional symbols according to new attribute values
+function updatePropSymbols(attribute){
+    map.eachLayer(function(layer){
+        if (layer.feature && layer.feature.properties[attribute]){
+            //update the layer style and popup
+            //access feature properties
+            var props = layer.feature.properties;
+
+            //update each feature's radius based on new attribute values
+            var radius = calcPropRadius(props[attribute]);
+            layer.setRadius(radius);
+
+            //add city to popup content string
+            var popupContent = "<p><b>State:</b> " + props.State + "</p>";
+
+            //add formatted attribute to panel content string
+            var year = attribute;
+            popupContent += "<p><b>Gross Sales of Hunting and Fishing Lisences in " + year + ":</b> " + props[attribute] + " million</p>";
+
+            //update popup content            
+            popup = layer.getPopup();            
+            popup.setContent(popupContent).update();
+        };
+    });
+};
+
+//Step 2: Import GeoJSON data
 function getData(){
     //load the data
-    fetch("data/MyPoints.geojson")
+    fetch("data/CorrectLisenceSales.geojson")
         .then(function(response){
             return response.json();
         })
-        // .then(function(json){
-        //     //create a Leaflet GeoJSON layer and add it to the map
-        //     L.geoJson(json, {
-        //         onEachFeature: onEachFeature
-        //     }).addTo(map);
-        // }) 
-        .then(function(json) {
-                //create marker options
-                var geojsonMarkerOptions = {
-                    radius: 8,
-                    fillColor: "#ff7800",
-                    color: "#000",
-                    weight: 1,
-                    opacity: 1,
-                    fillOpacity: 0.8
-                };
-                //using the pointToLayer method to turn any group of point features into
-                //their own layer onn the map, using parameters from the markeroptions var
-                L.geoJSON(json, {
-                    pointToLayer: function (feature, latlng) {
-                        return L.circleMarker(latlng, geojsonMarkerOptions);
-                    },
-                    onEachFeature: onEachFeature
-                }).addTo(map);
-             })
-       
+        .then(function(json){
+            var attributes = processData(json);
+            //calculate minimum data value
+            minValue = calculateMinValue(json);
+            //call function to create proportional symbols
+            createPropSymbols(json, attributes);
+            createSequenceControls(attributes);
+        })
 };
 
 document.addEventListener('DOMContentLoaded',createMap)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//added at Example 2.3 line 20...function to attach popups to each mapped feature
+// function onEachFeature(feature, layer) {
+//     //no property named popupContent; instead, create html string with all properties
+//     var popupContent = "";
+//     if (feature.properties) {
+//         //loop to add feature property names and values to html string
+//         for (var property in feature.properties){
+//             popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+//         }
+//         layer.bindPopup(popupContent);
+//     };
+
+// };
+
+//function to retrieve the data and place it on the map
+// function getData(){
+//     //load the data
+//     fetch("data/MyPoints.geojson")
+//         .then(function(response){
+//             return response.json();
+//         })
+//         .then(function(json) {
+//                 var attribute = " 2011";
+//                 //create marker options
+//                 var geojsonMarkerOptions = {
+//                     radius: 8,
+//                     fillColor: "#ff7800",
+//                     color: "#000",
+//                     weight: 1,
+//                     opacity: 1,
+//                     fillOpacity: 0.8
+//                 };
+//                 //using the pointToLayer method to turn any group of point features into
+//                 //their own layer onn the map, using parameters from the markeroptions var
+//                 L.geoJSON(json, {
+//                     pointToLayer: function (feature, latlng) {
+//                         var attValue = Number(feature.properties[attribute])
+//                         console.log(feature.properties, attValue)
+//                         return L.circleMarker(latlng, geojsonMarkerOptions);
+//                     },
+//                     //onEachFeature: onEachFeature
+//                 }).addTo(map);
+//              })
+       
+// };
+
+// document.addEventListener('DOMContentLoaded',createMap)
 
